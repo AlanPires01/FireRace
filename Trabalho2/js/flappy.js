4dd49deb7f609569d03915a04a1f64611374b84b
AlanPires01 <70494047+AlanPires01@users.noreply.github.com>
2023-07-08 11:21:37 -0300

    Adicionado obstaculos, elementos
    elementos - ganha ponto
    obstaculos - perde ponto

--- Trabalho2/js/flappy.js
+++ Trabalho2/js/flappy.js
@@ -13,6 +13,62 @@ function novoElemento(tagName, className) {
     return elemento
 }
 
+function Obstaculo(alturaJogo,larguraJogo,notificarPonto,passaro){
+    this.elemento = novoElemento('img', 'obstaculo')
+    this.elemento.src = './img/seagull.gif'
+    const imagensE = ['./img/vaca.png', './img/pedra.png','./img/angry-bird.png']; 
+    const direcaoX = [2.4,-2.4,0]
+    const incrementoY = [1,2,5]
+    let incrementoAtualY = 0
+    let incrementoAtualX = 0
+    let passou=false
+
+    this.setV = (visivel)=>this.elemento.style.display=visivel
+    
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+    this.getX = ()=>parseInt(this.elemento.style.left.split('px'[0]))
+    this.setX = x => this.elemento.style.left = `${x}px`
+    let indiceImagemAtual = 0;
+    let contador=0
+    this.animar = () => {
+        const novoY = this.getY() -incrementoY[incrementoAtualY]
+        const novoX = this.getX() - direcaoX[incrementoAtualX]
+        incrementoAtualY=(incrementoAtualY+1)%imagensE.length
+        
+        const alturaMaxima = alturaJogo - this.elemento.clientWidth
+        const larguraMaxima = larguraJogo - this.elemento.clientHeight
+        this.setY(novoY)
+        this.setX(novoX)
+       
+        
+       
+        if((novoX<=0)||(novoX>=larguraMaxima)||(novoY<=0)){
+            this.setV("none")
+        }
+        contador++
+       
+        
+        
+    }
+    const temporizador = setInterval(() => { 
+        if ((this.elemento.style.display.trim()==='none')) {
+        console.log("entrou")
+        this.setV('inline');
+        this.elemento.src = imagensE[indiceImagemAtual]
+        indiceImagemAtual = (indiceImagemAtual + 1) % imagens.length;
+        this.setY(alturaJogo/2.3)
+        this.setX(larguraJogo / 2.2)
+
+       
+        incrementoAtualX=(incrementoAtualX+1)%imagens.length
+        contador=0
+
+        }
+    },11000)
+    this.setY(alturaJogo/2.3)
+        this.setX(larguraJogo / 2.2)
+}
 
 
 function Elemento(alturaJogo,larguraJogo,notificarPonto,passaro){
@@ -44,19 +100,7 @@ function Elemento(alturaJogo,larguraJogo,notificarPonto,passaro){
         this.setX(novoX)
        
         
-        if ((this.elemento.style.display.trim()==='none')&&(contador>=100)) {
-            console.log("entrou")
-            this.setV('inline');
-            this.elemento.src = imagensE[indiceImagemAtual]
-            indiceImagemAtual = (indiceImagemAtual + 1) % imagens.length;
-            this.setY(alturaJogo/2.3)
-            this.setX(larguraJogo / 2.2)
-
-           
-            incrementoAtualX=(incrementoAtualX+1)%imagens.length
-            contador=0
-
-        }
+       
         if((novoX<=0)||(novoX>=larguraMaxima)||(novoY<=0)){
             this.setV("none")
         }
@@ -65,6 +109,21 @@ function Elemento(alturaJogo,larguraJogo,notificarPonto,passaro){
         
         
     }
+    const temporizador = setInterval(() => { 
+        if ((this.elemento.style.display.trim()==='none')) {
+        console.log("entrou")
+        this.setV('inline');
+        this.elemento.src = imagensE[indiceImagemAtual]
+        indiceImagemAtual = (indiceImagemAtual + 1) % imagens.length;
+        this.setY(alturaJogo/2.3)
+        this.setX(larguraJogo / 2.2)
+
+       
+        incrementoAtualX=(incrementoAtualX+1)%imagens.length
+        contador=0
+
+        }
+    },10000)
     this.setY(alturaJogo/2.3)
         this.setX(larguraJogo / 2.2)
 }
@@ -76,10 +135,12 @@ function Rival (alturaJogo,larguraJogo,notificarPonto) {
     this.elemento = novoElemento('img', 'rival')
     this.elemento.src = 'img/carro-verde.png'
     const imagens = ['./img/carro-azul.png', './img/carro-branco.png','./img/carro-verde.png']; 
-    const direcaoX = [2.4,-2.4,0]
-    const incrementoY = [3,4,5]
+    const direcaoX = [2.4,-2.10,0,2.2,-2.2,0.5]
+    const incrementoY = [3,4,5,2,1,6]
     let incrementoAtualY = 0
     let incrementoAtualX = 0
+    const valoresIniciaisY=[alturaJogo/2.1,alturaJogo/2.3,alturaJogo/2.4]
+    const valoresIniciaisX=[larguraJogo/2,larguraJogo/2.3,larguraJogo/1.8]
 
     this.setV = (visivel)=>this.elemento.style.display=visivel
     
@@ -92,7 +153,7 @@ function Rival (alturaJogo,larguraJogo,notificarPonto) {
         const novoY = this.getY() -incrementoY[incrementoAtualY]
         const novoX = this.getX() - direcaoX[incrementoAtualX]
 
-        incrementoAtual=(incrementoAtualY+1)%imagens.length
+        incrementoAtualY=Math.floor(Math.random() * incrementoY.length)
         
         const alturaMaxima = alturaJogo - this.elemento.clientWidth
         const larguraMaxima = larguraJogo - this.elemento.clientHeight
@@ -101,21 +162,23 @@ function Rival (alturaJogo,larguraJogo,notificarPonto) {
         if (novoY <= 0) {
             notificarPonto()
             this.setV("none")
+            
+            indiceImagemAtual = Math.floor(Math.random() * imagens.length); // Índice aleatório para as imagens
             this.elemento.src = imagens[indiceImagemAtual]
-            indiceImagemAtual = (indiceImagemAtual + 1) % imagens.length;
-            this.setY(alturaJogo/2.3)
-            this.setX(larguraJogo / 2.2)
+            console.log("indice" + indiceImagemAtual)
+            this.setY(valoresIniciaisY[Math.floor(Math.random() * valoresIniciaisY.length)])
+            this.setX(valoresIniciaisX[Math.floor(Math.random() * valoresIniciaisX.length)])
 
             this.setV("inline")
-            incrementoAtualX=(incrementoAtualX+1)%imagens.length
+            incrementoAtualX=Math.floor(Math.random() * direcaoX.length)
 
         }
         
     }
 
-    this.setY(alturaJogo / 2.3)
-    this.setX(larguraJogo / 2.2)
-
+    this.setY(alturaJogo / 2)
+    this.setX(Math.floor(Math.random() * valoresIniciaisX.length))
+ 
    
 }
 
@@ -130,14 +193,13 @@ let sentidoCurva //variavel para indicar sentido da curva 0 - nao tem curva, 1-c
 const imagens = ['./img/curva01.png', './img/curva02.png']; // Array com os caminhos das imagens
 let trocar=0
 function background(area, pontos) {
-    console.log(pontos)
     let indiceImagemAtual = 0;
     function trocarImagem() {
-      if (trocar<=10) {
+      if (trocar<=20) {
         dia();
-      }else if(trocar<=20) {
+      }else if(trocar<=40) {
         noite()
-      }else if(trocar<=30){
+      }else if(trocar<=60){
         curva()
       }else{
         trocar=0
@@ -166,7 +228,7 @@ function background(area, pontos) {
       sentidoCurva=0
     }
   
-    setInterval(trocarImagem, 500);
+    setInterval(trocarImagem, 700);
   }
   
 function Passaro(alturaJogo,larguraJogo) {
@@ -210,14 +272,19 @@ function Passaro(alturaJogo,larguraJogo) {
    
    
     let tempoAtual
-   let velocidade =5
+   let foraDaPista=false
+   let velocidade = 20
+
+  
     this.animar = () => {
         
         const novoY = this.getY() + (voando ? 8 : -5)
         const novoX = this.getX() + (esquerda ? -velocidade : 0) + (direita ? velocidade : 0) + sentidoCurva
-        
+        velocidade=((foraDaPista)&&(velocidade>=2) ? velocidade*0.95 : velocidade)//verifica se o carro está fora da pista
         const alturaMaxima = alturaJogo - this.elemento.clientWidth
         const larguraMaxima = larguraJogo - this.elemento.clientHeight
+        const limitePistaDireita = larguraMaxima-250
+        const limitePistaEsquerda = 100
         if (novoY <= 0) {
             this.setY(0)
         } else if (novoY >= alturaMaxima) {
@@ -225,22 +292,24 @@ function Passaro(alturaJogo,larguraJogo) {
         } else {
             this.setY(novoY)
         }
-        if (novoX <= 160) {//se for menor está colidindo com o meio fio a esquerda
+        if ((novoX < limitePistaEsquerda)&&(novoX>=0)) {//se for menor está colidindo com o meio fio a esquerda---> FORA DA PISTA
             
-            this.setX(160)
-            velocidade=2
-            tempoAtual=tempo
+            
+            (novoX<=0 ? this.setX(0):this.setX(novoX))
+            foraDaPista=true
+            console.log("velocidade= "+ velocidade)
 
-        } else if (novoX >= larguraMaxima-250) {//se for maior está colidindo com o meio fio a direita
-            time=1500
-            this.setX(larguraMaxima-250)
-            velocidade=2
-            tempoAtual=tempo
+        } else if ((novoX > limitePistaDireita)&&(novoX<=larguraMaxima)) {//se for maior está colidindo com o meio fio a direita-->FORA DA PISTA
+            
+            (novoX>=larguraMaxima ? this.setX(larguraMaxima):this.setX(novoX))
+            foraDaPista=true
+            console.log("velocidade= "+ velocidade)
+           
         } else {
             this.setX(novoX)
-            if(tempo-tempoAtual>5){
-              velocidade=10
-            }
+            foraDaPista=false
+            velocidade=10
+           
         }
 
     }
@@ -297,9 +366,13 @@ function colidiu(passaro, rival) {
     const progresso = new Progresso()
 
     const passaro = new Passaro(altura,largura)
-    const rival = new Rival(altura, largura,        
+    const rival01 = new Rival(altura, largura,        
         () => progresso.atualizarPontos(++pontos))
+        const rival02 = new Rival(altura, largura,        
+             () => progresso.atualizarPontos(++pontos))
     const elemento = new Elemento(altura, largura,        
+        () => progresso.atualizarPontos(++pontos),passaro)
+    const obstaculo = new Obstaculo(altura, largura,        
         () => progresso.atualizarPontos(++pontos),passaro)
 
     
@@ -309,22 +382,36 @@ function colidiu(passaro, rival) {
     // areaDoJogo.appendChild(musica);
     areaDoJogo.appendChild(progresso.elemento) 
     areaDoJogo.appendChild(passaro.elemento)
-    areaDoJogo.appendChild(rival.elemento)
+    areaDoJogo.appendChild(rival01.elemento)
+    areaDoJogo.appendChild(rival02.elemento)
     areaDoJogo.appendChild(elemento.elemento)
+    areaDoJogo.appendChild(obstaculo.elemento)
 
    /*  barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento)) */
 
     this.start = () => {
         const temporizador = setInterval(() => {
            /*  barreiras.animar() */
-            rival.animar()
-            passaro.animar()
+           rival01.animar()
+             rival02.animar()
             elemento.animar()
-               if(colidiu(passaro,rival)){
+            obstaculo.animar()
+            passaro.animar()
+               if(colidiu(passaro,rival01)){
                  clearInterval(temporizador) 
                 }  
+                if(colidiu(passaro,rival02)){
+                   clearInterval(temporizador) 
+                  }  
                 if (colidiu(passaro,elemento)) {
                     progresso.atualizarPontos(pontos+2)
+
+                    elemento.elemento.style.display="none"
+    
+                    
+                }
+                if (colidiu(passaro,obstaculo)) {
+                    progresso.atualizarPontos(pontos-2)
 
                     elemento.elemento.style.display="none"
     
@@ -337,6 +424,7 @@ function colidiu(passaro, rival) {
               });
         }, 20)
         background(areaDoJogo,pontos)
+
     }
 }
  new FlappyBird().start()
